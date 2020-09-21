0d110cfdb79b30e29b2980db4a0aef79d8fc3381
Rasmus Rask <35446411+SaintRask@users.noreply.github.com>
2020-09-21 11:33:36 +0200

    chore: Mongodb connection

---
+++ API/Controllers/LabyrinthController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Models;
+using API.Services;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LabyrinthController : ControllerBase
+    {
+        private readonly LabyrinthService _labyrinthService;
+
+        public LabyrinthController(LabyrinthService labyrinthService)
+        {
+            _labyrinthService = labyrinthService;
+        }
+
+        // GET: api/<LabyrinthController>
+        [HttpGet]
+        public ActionResult<List<Labyrinth>> Get() =>
+            _labyrinthService.Get();
+
+        // GET api/<LabyrinthController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Labyrinth> Get(string id)
+        {
+            var labyrinth = _labyrinthService.Get(id);
+
+            if (labyrinth == null)
+                return NotFound();
+
+            return labyrinth;
+        }
+
+        // POST api/<LabyrinthController>
+        [HttpPost]
+        public ActionResult<Labyrinth> Create(Labyrinth labyrinth)
+        {
+            _labyrinthService.Create(labyrinth);
+
+            return CreatedAtRoute("GetLabyrinth", new { id = labyrinth.Id.ToString() }, labyrinth);
+        }
+
+        // PUT api/<LabyrinthController>/5
+        [HttpPut("{id}")]
+        public IActionResult Update(string id, Labyrinth labyrinthIn)
+        {
+            var labyrinth = _labyrinthService.Get(id);
+
+            if (labyrinth == null)
+                return NotFound();
+
+            _labyrinthService.Update(id, labyrinthIn);
+
+            return NoContent();
+        }
+
+        // DELETE api/<LabyrinthController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            var labyrinth = _labyrinthService.Get(id);
+
+            if (labyrinth == null)
+                return NotFound();
+
+            _labyrinthService.Remove(labyrinth.Id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ API/Services/LabyrinthService.cs
@@ -0,0 +1,43 @@
+﻿using API.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Services
+{
+    public class LabyrinthService
+    {
+        private readonly IMongoCollection<Labyrinth> _labyrinths;
+
+        public LabyrinthService(ILabyrinthDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _labyrinths = database.GetCollection<Labyrinth>(settings.LabyrinthsCollectionName);
+        }
+
+        public List<Labyrinth> Get() =>
+            _labyrinths.Find(labyrinth => true).ToList();
+
+        public Labyrinth Get(string id) =>
+            _labyrinths.Find<Labyrinth>(labyrinth => labyrinth.Id == id).FirstOrDefault();
+
+        public Labyrinth Create(Labyrinth labyrinth)
+        {
+            _labyrinths.InsertOne(labyrinth);
+            return labyrinth;
+        }
+
+        public void Update(string id, Labyrinth labyrinthIn) =>
+            _labyrinths.ReplaceOne(labyrinth => labyrinth.Id == id, labyrinthIn);
+
+        public void Remove(Labyrinth labyrinthIn) =>
+            _labyrinths.DeleteOne(labyrinth => labyrinth.Id == labyrinthIn.Id);
+
+        public void Remove(string id) =>
+            _labyrinths.DeleteOne(labyrinth => labyrinth.Id == id);
+    }
+}
